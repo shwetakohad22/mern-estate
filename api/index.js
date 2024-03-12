21fd40009f43b17b4fe92680a205cb7f307b9946
shwetakohad22 <shwetakohad22@gmail.com>
2024-03-12 11:23:10 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to DB!!!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 app.listen(3000, () => {
-  console.log("server is running!!!");
+  console.log("server is running on PORT 3000!!!");
 });
+
